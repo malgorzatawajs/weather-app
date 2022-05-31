b6b87928b77859cb107a5316411da87f25051d2b
Małgorzata <mawajs@o2.pl>
2022-05-31 18:47:14 +0200

    Added current weather icon

--- src/script.js
+++ src/script.js
@@ -39,6 +39,11 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#temp-description").innerHTML =
     response.data.weather[0].description;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function searchCity(city) {
